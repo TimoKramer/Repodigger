5d613ba9dbee85258df9bc734d424a48b268112a
TimoKramer <ich@tinokramer.de>
2015-06-04 16:57:41 +0200

    trying kivy but pycharm not working

---
+++ kivy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+__author__ = 'Timo Kramer'
+__immanr__ = '20119022'
+
+import kivy, github
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+
+kivy.require('1.0.6')
+
+
+class MainScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='Dummy'))
+        self.raw_json = Label(text=github.GithubRequest('timokramer/PatFra'))
+        self.add_widget(self.raw_json)
+
+class MyApp(App):
+
+    def build(self):
+        return MainScreen()
+
+if __name__ == '__main__':
+    MyApp().run()
