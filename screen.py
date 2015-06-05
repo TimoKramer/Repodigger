584dc45d2f98dbb1cc63f13afb1b330f844c8728
TimoKramer <ich@tinokramer.de>
2015-06-05 15:54:55 +0200

    Starts with BoxLayout

--- screen.py
+++ screen.py
@@ -4,10 +4,14 @@
 __author__ = 'Timo Kramer'
 __immanr__ = '20119022'
 
-import kivy, github, json
+import kivy, data
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 kivy.require('1.0.6')
 
@@ -17,14 +21,30 @@ class MainScreen(GridLayout):
         super(MainScreen, self).__init__(**kwargs)
         self.cols = 2
         self.add_widget(Label(text='Dummy'))
-        issues = github.GithubRequest('timokramer/PatFra').return_json()
-        self.raw_json = Label(text='issues')
+        issues = data.GithubRequest('timokramer/repodigger').return_structured_data()
+        self.raw_json = Label(text=issues[0])
         self.add_widget(self.raw_json)
 
-class MyApp(App):
 
+class LoginScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.size_hint = (0.5, 0.5)
+        self.cols = 2
+        self.add_widget(Label(text='Insert Repo'))
+        self.add_widget(TextInput(text='e.g. "timokramer/repodigger"'))
+        self.add_widget(Button(text='OK'))
+
+
+class Screen(AnchorLayout):
+    def __init__(self, **kwargs):
+        super(Screen, self).__init__(**kwargs)
+        self.add_widget(LoginScreen())
+
+
+class MainApp(App):
     def build(self):
-        return MainScreen()
+        return Screen()
 
 if __name__ == '__main__':
-    MyApp().run()
+    MainApp().run()
