f0fa8873b8b3ec74a3739759984631789d4a6ad2
TimoKramer <ich@tinokramer.de>
2015-06-12 12:57:35 +0200

    enter und okbutton funktionieren

--- screen.py
+++ screen.py
@@ -4,21 +4,42 @@
 __author__ = 'Timo Kramer'
 __immanr__ = '20119022'
 
+
 from kivy.app import App
+from kivy.event import EventDispatcher
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 
 
 Builder.load_file('screen.kv')
 
-class RootWidget(FloatLayout):
+
+class RepodiggerApp(App):
+
+    def build(self):
+        return RepodiggerController()
+
+
+class RepodiggerController(FloatLayout):
+    def build(self, **kwargs):
+        self.register_event_type('on_ok')
+        super(RepodiggerController, self).__init__(**kwargs)
+
+    def check_status(self):
+        print('button statis is: {state}'.format(state=self.ids.ok_button.state))
+        print('text input text is: {txt}'.format(txt=self.ids.text_input.text))
+
+
+class LoginScreen(BoxLayout):
+
+    def get_input(self, input):
+        print(input)
+
+
+class MyBackground(FloatLayout):
     pass
 
 
-class MainApp(App):
-
-    def build(self):
-        return RootWidget()
-
 if __name__ == '__main__':
-    MainApp().run()
+    RepodiggerApp().run()
