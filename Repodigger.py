ab6db193c774ea476a03a0a49417f119dc6099a7
TimoKramer <ich@tinokramer.de>
2015-06-19 10:32:51 +0200

    Sackgasse

--- Repodigger.py
+++ Repodigger.py
@@ -7,10 +7,16 @@ __immanr__ = '20119022'
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.listview import ListView
+from kivy.uix.listview import ListView, ListItemButton
+from kivy.uix.selectableview import SelectableView
 from kivy.properties import ObjectProperty, ListProperty
 from kivy.network.urlrequest import UrlRequest
+from kivy.adapters.dictadapter import DictAdapter
+from kivy.factory import Factory
 import re
+
+Factory.register('SelectableView', cls=SelectableView)
+Factory.register('ListItemButton', cls=ListItemButton)
 
 
 class RepodiggerApp(App):
@@ -51,7 +57,7 @@ class LoginScreen(Screen):
     def parse_request(self, req, results):
         print('Succeeded requesting Github Issues')
         self.parent.current = 'Issue Screen'
-        self.parent.get_screen('Issue Screen').build_issue_widgets(results)
+        self.parent.get_screen('Issue Screen')
 
     def parse_failure(self, req, result):
         print('There was a problem: "', result['message'], '"')
@@ -60,17 +66,31 @@ class LoginScreen(Screen):
         print('There was an error: ', error)
 
 
-class IssueScreen(Screen, ListView):
+class IssueScreen(Screen):
     item_strings = ListProperty(None)
+    issues = ListProperty(None)
 
     def __init__(self, **kwargs):
-        super(IssueScreen, self).__init__(
-            item_strings=self.item_strings
-        )
+        kwargs['cols'] = 1
+        super(IssueScreen, self).__init__(**kwargs)
+        print("Erfolg!")
+        list_item_args_converter = lambda row_index, rec: {'title': rec['title'],
+                                                           'is_selected': rec['is_selected'],
+                                                           'size_hint_y': None,
+                                                           'height': 25}
+        dict_adapter = DictAdapter(sorted_keys=[str(i) for i in self.issues],
+                                   data=self.issues,
+                                   args_converter=list_item_args_converter,
+                                   template='CustomListItem')
+        list_view = ListView(item_strings=[str(issues) for issues in self.item_strings])
+        print(item for item in self.item_strings)
+
+        self.add_widget(list_view)
 
     def build_issue_widgets(self, issues):
+        self.issues = issues
         for issue in issues:
-            print(issue['title'])
+            #print(issue['title'])
             self.item_strings.append(issue['title'])
 
 
--- gh.py
+++
@@ -1,7 +0,0 @@
-from github import Github
-
-g = Github("timokramer", "gitte_und_huber3")
-
-for repo in g.get_user().get_repos():
-    print(repo.name)
-    repo.edit(has_wiki=False)
