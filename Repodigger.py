87ad802fa0f785ec85e6c366fd7caa47bf2c3e43
TimoKramer <ich@timokramer.de>
2015-06-19 20:51:21 +0200

    working but unnice

--- Repodigger.py
+++ Repodigger.py
@@ -7,19 +7,40 @@ __immanr__ = '20119022'
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.listview import ListItemButton
 from kivy.properties import ObjectProperty, ListProperty
 from kivy.network.urlrequest import UrlRequest
 import re
 
 
 class RepodiggerApp(App):
+
     def build(self):
-        return Manager()
+        root = Manager()
+        global global_screen_manager
+        global_screen_manager = root
+        return root
 
 
 class Manager(ScreenManager):
     login_screen = ObjectProperty(None)
     issue_screen = ObjectProperty(None)
+    detail_screen = ObjectProperty(None)
+    burndown_screen = ObjectProperty(None)
+
+
+class DetailScreen(Screen):
+
+    def __init__(self, **kwargs):
+        super(DetailScreen, self).__init__(**kwargs)
+
+    def populate_details(self):
+        pass
+
+
+class BurndownScreen(Screen):
+    def __init__(self, **kwargs):
+        super(BurndownScreen, self).__init__(**kwargs)
 
 
 class LoginScreen(Screen):
@@ -49,8 +70,8 @@ class LoginScreen(Screen):
 
     def parse_request(self, req, results):
         print('Succeeded requesting Github Issues')
-        self.parent.current = 'Issue Screen'
-        self.parent.get_screen('Issue Screen').build_issue_widgets(results)
+        global_screen_manager.current = 'Issue Screen'
+        global_screen_manager.get_screen('Issue Screen').build_issue_widgets(results)
 
     def parse_failure(self, req, result):
         print('There was a problem: "', result['message'], '"')
@@ -60,7 +81,7 @@ class LoginScreen(Screen):
 
 
 class IssueScreen(Screen):
-    item_strings = ListProperty([])
+    my_item_strings = ListProperty([])
 
     def __init__(self, **kwargs):
         kwargs['cols'] = 1
@@ -69,14 +90,31 @@ class IssueScreen(Screen):
     def build_issue_widgets(self, issues):
         for issue in issues:
             if issue['state'] == 'open':
-                self.item_strings.append(issue['title'])
-        self.issues_list.item_strings = self.item_strings
+                self.my_item_strings.append(issue['title'])
+        self.issues_list.item_strings = self.my_item_strings
+        self.issues_list.adapter.data.clear()
+        self.issues_list.adapter.data.extend(self.my_item_strings)
+        self.issues_list._trigger_reset_populate()
 
     def on_burndown_press(self):
-        print("Fine!")
+        global_screen_manager.current = 'Burndown Screen'
 
     def on_change_press(self):
-        print("Fine!")
+        global_screen_manager.current = 'Login Screen'
+
+    def on_detail_press(self):
+        global_screen_manager.current = 'Detail Screen'
+        global_screen_manager.get_screen('Detail Screen').populate_details()
+
+
+class IssueButton(ListItemButton):
+    def on_detail_press(self):
+        print(self.parent)
+        print(self.parent.parent)
+        print(self.parent.parent.parent)
+        print(self.parent.parent.parent.parent)
+        print(self.parent.parent.parent.parent.parent)
+        self.parent.parent.parent.parent.parent.on_detail_press()
 
 
 if __name__ == '__main__':
